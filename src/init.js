f55eaccb82b46c7b5f312f636ca611b465e64de7
shren <renshihong@hotmail.com>
2024-12-11 14:56:04 +0800

    hand-landmark

--- src/init.js
+++ src/init.js
@@ -1,4 +1,67 @@
 import { instantiateFaustModule, LibFaust, FaustCompiler, FaustMonoDspGenerator } from "@grame/faustwasm/dist/esm-bundle";
+import { FilesetResolver, HandLandmarker, DrawingUtils } from "@mediapipe/tasks-vision";
+
+let node;
+export const initHandLandmarker = async () => {
+  const vision = await FilesetResolver.forVisionTasks("./wasm");
+  const handLandmarker = await HandLandmarker.createFromOptions(
+    vision,
+    {
+      baseOptions: {
+        modelAssetPath: "hand_landmarker.task"
+      },
+      numHands: 2,
+      runningMode: "VIDEO"
+    }
+  );
+  console.log(handLandmarker);
+  // const devices = await navigator.mediaDevices.enumerateDevices();
+  // console.log(devices);
+  /** @type {HTMLVideoElement} */
+  const video = document.getElementById("camera");
+  /** @type {HTMLCanvasElement} */
+  const canvasElement = document.getElementById("landmarks");
+  const canvasCtx = canvasElement.getContext("2d");
+  const drawingUtils = new DrawingUtils(canvasCtx);
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  console.log(stream);  
+
+  let lastVideoTime = -1;
+  let results;
+  const renderLoop = () => {
+    if (video.currentTime !== lastVideoTime) {
+      // const timestamp = Date.now();
+      const timestamp = performance.now();
+      results = handLandmarker.detectForVideo(video, timestamp);
+      // processResults(detections);
+      // console.log(results);
+      if (results && node) {
+        if (results.landmarks[0]) {
+          node.setParamValue("/Djembe/time", results.landmarks[0][0].x * 9000 + 1000);
+        }
+      }
+      lastVideoTime = video.currentTime;
+    }
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    if (results && results.landmarks) {
+      for (const landmarks of results.landmarks) {
+        drawingUtils.drawConnectors(landmarks, HandLandmarker.HAND_CONNECTIONS, { color: "#0000FF", lineWidth: 5 });
+        drawingUtils.drawLandmarks(landmarks, { color: "#FFFFFF", lineWidth: 2 });
+      }
+    }
+    canvasCtx.restore();
+    requestAnimationFrame(renderLoop);
+  }
+  video.addEventListener("loadeddata", renderLoop);
+  video.srcObject = stream;
+  await video.play();
+  canvasElement.style.width = video.videoWidth;
+  canvasElement.style.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvasElement.height = video.videoHeight;
+};
 
 export const init = async () => {
   const faustModule = await instantiateFaustModule();
@@ -13,10 +76,12 @@ export const init = async () => {
   const argv = ["-I", "libraries/"];
   const code = `
 import("stdfaust.lib");
-process = ba.pulsen(1, 10000) : pm.djembe(60, 0.3, 0.4, 1);
+interval = hslider("time", 10000, 1000, 10000, 1);
+process = ba.pulsen(1, interval) : pm.djembe(60, 0.3, 0.4, 1);
 `;
   // Compile the DSP
   await generator.compile(compiler, name, code, argv.join(" "));
-  const node = await generator.createNode(audioContext);
+  node = await generator.createNode(audioContext);
   node.connect(audioContext.destination);
+  console.log(node.getParams());
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
-import { init } from './init.js'
+import { init, initHandLandmarker } from './init.js'
 import { appContent } from './app-content.js';
 
 document.querySelector('#app').innerHTML = appContent;
 document.querySelector('#play').addEventListener("click", init);
+initHandLandmarker();
